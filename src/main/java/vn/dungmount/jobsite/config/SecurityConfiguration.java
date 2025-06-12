c90b3baced6cfe992b40516a043b5973bcf6f319
taidungit <dungmount75@gmail.com>
2025-06-12 22:30:36 +0700

    jwt exception

--- src/main/java/vn/dungmount/jobsite/config/SecurityConfiguration.java
+++ src/main/java/vn/dungmount/jobsite/config/SecurityConfiguration.java
@@ -16,6 +16,10 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
@@ -33,7 +37,7 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http,CustomAuthenticationEntryPoint caep) throws Exception {
         http
         .csrf(c->c.disable())       
         .authorizeHttpRequests(
@@ -41,12 +45,31 @@ public class SecurityConfiguration {
                                 .requestMatchers("/","/login").permitAll()
                                 
                                 .anyRequest().authenticated())
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
+                .authenticationEntryPoint(caep)
+                )
+                .exceptionHandling(
+                 exceptions -> exceptions
+                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()) //401
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler())) //403
+
                 .formLogin(f->f.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("dungmount");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
 
     @Bean
     public JwtDecoder jwtDecoder() {
--- src/main/java/vn/dungmount/jobsite/util/error/GlobalException.java
+++ src/main/java/vn/dungmount/jobsite/util/error/GlobalException.java
@@ -18,15 +18,15 @@ import vn.dungmount.jobsite.domain.RestResponse;
 @RestControllerAdvice
 public class GlobalException {
               @ExceptionHandler(value = {
-            IdInvalidException.class,
+            // IdInvalidException.class,
             UsernameNotFoundException.class,
             BadCredentialsException.class
     })
-    public ResponseEntity<RestResponse<Object>> handleIdException(IdInvalidException idException) {
+    public ResponseEntity<RestResponse<Object>> handleIdException(Exception ex) {
           RestResponse<Object> res=new RestResponse<Object>();
             res.setStatusCode(HttpStatus.BAD_REQUEST.value());
-            res.setError(idException.getMessage());
-            res.setMessage("IdInvalidException");
+            res.setError(ex.getMessage());
+            res.setMessage("Exception occures....");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
